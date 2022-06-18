546b7e5310f1ea5ad76da862a6e89dfe5678a7e9
rainoftime <rainoftime@gmail.com>
2022-06-18 17:16:35 +0800

    seems the signal handler has some issues...

--- prover.py
+++ prover.py
@@ -120,8 +120,8 @@ def solve_sygus_file(filename: str, prover="all"):
     Currently, we support the sygus frontend
     """
     # FIXME: To use abstract domains and to preserve completeness,
-    # FIXME: I cast integer variables to reals (this can be bad?) when parsing.
-    # FIXME: A better idea is to transform the transition system after the parsing
+    #   I cast integer variables to reals (this can be bad?) when parsing.
+    #   A better idea is to transform the transition system after the parsing
     all_vars, init, trans, post = parse_sygus(filename, to_real_type=True)
     sts = TransitionSystem()
     sts.from_z3_cnts([all_vars, init, trans, post])
@@ -147,16 +147,12 @@ if __name__ == "__main__":
     import argparse
 
     # solve_chc_file("./benchmarks/bv/simple.smt2", "efsmt")
-    solve_chc_file("/Users/prism/Work/eldarica-bin/tests/sygus/fib_44.sl.smt2", "efsmt")
+    # solve_chc_file("/Users/prism/Work/eldarica-bin/tests/sygus/fib_44.sl.smt2", "efsmt")
 
     # solve_sygus_file("./benchmarks/sygus-inv/LIA/2017.ASE_FiB/minor1.sl", "symabs")
     # solve_sygus_file("./benchmarks/sygus-inv/LIA/2017.ASE_FiB/minor1.sl", "efsmt")
     # fib_04.sl needs disjunctive?
-    # solve_sygus_file("/Users/prism/Work/logicbox/independent/efsmt/benchmarks/sygus-inv/LIA/2017.ASE_FiB/fib_01.sl", "efsmt")
-    # solve_sygus_file("/Users/prism/Work/logicbox/independent/efsmt/benchmarks/sygus-inv/LIA/2018.NeurIPS_Code2Inv/38.c.sl", "efsmt")
-    # solve_sygus_file("./benchmarks/sygus-inv/LIA/2017.ASE_FiB/vardep.sl")
-    # solve_sygus_file("/Users/prism/Work/logicbox/independent/efsmt/benchmarks/sygus-inv/LIA/2017.ASE_FiB/fib_35.sl", "symabs")
-    # solve_sygus_file("/Users/prism/Work/logicbox/independent/efsmt/benchmarks/sygus-inv/LIA/2017.ASE_FiB/fib_35.sl", "efsmt")
+    solve_sygus_file('/Users/prism/Work/efmc/benchmarks/sygus-inv/LIA/2017.ASE_FiB/fib_30_x.sl', "pdr")
     exit(0)
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', dest='file', default='none', type=str, help="file")
