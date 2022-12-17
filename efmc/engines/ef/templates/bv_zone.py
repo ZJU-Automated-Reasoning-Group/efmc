2c3eb85be177a81011415f2966db029a6fe4ea05
rainoftime <rainoftime@gmail.com>
2022-12-17 21:19:42 +0800

    disjunctive zone and octagon?

--- efmc/engines/ef/templates/bv_zone.py
+++ efmc/engines/ef/templates/bv_zone.py
@@ -7,7 +7,7 @@ import z3
 from efmc.sts import TransitionSystem
 from efmc.engines.ef.templates.abstract_template import TemplateType, Template
 from efmc.engines.ef.templates.bv_utils import Signedness
-from efmc.utils import get_variables, big_and
+from efmc.utils import get_variables, big_and, big_or
 
 
 class BitVecZoneTemplate(Template):
@@ -98,11 +98,11 @@ class BitVecZoneTemplate(Template):
             term_u = template_vars_for_term[1]  # upper bound of the term
 
             if self.signedness == Signedness.UNSIGNED:
-                cnts.append(z3.And(model[term_l] <= term, model[term_u] >= term))
-            else:
                 cnts.append(z3.And(z3.ULE(model[term_l], term), z3.UGE(model[term_u], term)))
+            else:
+                cnts.append(z3.And(model[term_l] <= term, model[term_u] >= term))
 
-        return z3.And(cnts)
+        return big_and(cnts)
 
 
 class DisjunctiveBitVecZoneTemplate(Template):
@@ -116,6 +116,13 @@ class DisjunctiveBitVecZoneTemplate(Template):
         self.sts = sts
         self.arity = len(self.sts.variables)
 
+        self.zones = []  # x - y, x - z, y - z, ...
+        for x, y in list(itertools.combinations(self.sts.variables, 2)):
+            self.zones.append(x - y)
+        self.prime_zones = []
+        for px, py in list(itertools.combinations(self.sts.prime_variables, 2)):
+            self.prime_zones.append(px - py)
+
         self.template_vars = []  # vector of vector
         self.template_index = 0  # number of templates
 
@@ -123,7 +130,7 @@ class DisjunctiveBitVecZoneTemplate(Template):
 
         self.add_template_vars()
 
-        # pre compute to reduce redundant calling
+        # pre-compute to reduce redundant calling
         self.template_cnt_init_and_post = None
         self.template_cnt_trans = None
         self.add_template_cnts()
@@ -132,22 +139,84 @@ class DisjunctiveBitVecZoneTemplate(Template):
         """
         Add several groups of template vars
         """
-        raise NotImplementedError
+        for i in range(self.num_of_disjuncts):
+            vars_for_dis = []
+            for j in range(len(self.zones)):
+                term = self.zones[i]
+                term_vars = get_variables(term)
+                term_name = "{}{}".format(term_vars[0], term_vars[1])
+                self.template_index += 1
+                tvars = [z3.BitVec("d{0}_{1}_l".format(i, term_name), term.size()),
+                         z3.BitVec("d{0}_{1}_u".format(i, term_name), term.size())]
+                vars_for_dis.append(tvars)
+
+            self.template_vars.append(vars_for_dis)
+        print(self.template_vars)
+        # raise NotImplementedError
 
     def get_additional_cnts_for_template_vars(self):
         """
         This implementation does not need additional ones?
         """
-        raise NotImplementedError
+        raise z3.BoolVal(True)
 
     def add_template_cnts(self):
         """
         Add cnts for init and post assertions (a trick)
         """
-        raise NotImplementedError
+        cnt_init_and_post_dis = []
+        cnt_trans_dis = []
+
+        for i in range(self.num_of_disjuncts):
+            # Invariant: len(self.template_vars) = self.num_of_disjuncts
+            vars_for_ith_disjunct = self.template_vars[i]
+            cnt_init_post = []  # For sts.variables
+            cnt_trans = []  # For sts.prime_variables
+            for j in range(len(self.zones)):
+                term = self.zones[j]  # e.g., x - y
+                term_prime = self.prime_zones[j]  # e.g., x' - y'
+                template_vars_for_term = vars_for_ith_disjunct[j]
+                term_l = template_vars_for_term[0]  # lower bound of the term
+                term_u = template_vars_for_term[1]  # upper bound of the term
+
+                if self.signedness == Signedness.UNSIGNED:
+                    cnt_init_post.append(z3.And(z3.ULE(term_l, term), z3.UGE(term_u, term)))
+                    cnt_trans.append(z3.And(z3.ULE(term_l, term_prime),
+                                            z3.UGE(term_u, term_prime)))
+                else:
+                    cnt_init_post.append(z3.And(term_l <= term, term_u >= term))
+                    cnt_trans.append(z3.And(term_l <= term_prime,
+                                            term_u >= term_prime))
+
+            cnt_init_and_post_dis.append(big_and(cnt_init_post))
+            cnt_trans_dis.append(big_and(cnt_trans))
+
+        self.template_cnt_init_and_post = big_or(cnt_init_and_post_dis)
+        self.template_cnt_trans = big_or(cnt_trans_dis)
+        # print(self.template_cnt_init_and_post)
+        # print(self.template_cnt_trans)
 
     def build_invariant_expr(self, model: z3.ModelRef, use_prime_variables=False):
         """
         Build an invariant from a model (fixing the values of the template vars)
         """
-        raise NotImplementedError
+        cnts_dis = []
+        for i in range(self.num_of_disjuncts):
+            # Invariant: len(self.template_vars) = self.num_of_disjuncts
+            vars_for_ith_disjunct = self.template_vars[i]
+            cnts = []  # constraints for one disjunct
+            for j in range(len(self.zones)):
+                if use_prime_variables:
+                    term = self.prime_zones[j]
+                else:
+                    term = self.zones[j]
+                template_vars_for_term = vars_for_ith_disjunct[j]
+                term_l = template_vars_for_term[0]  # lower bound of the term
+                term_u = template_vars_for_term[1]  # upper bound of the term
+                if self.signedness == Signedness.UNSIGNED:
+                    cnts.append(z3.And(z3.ULE(model[term_l], term), z3.UGE(model[term_u], term)))
+                else:
+                    cnts.append(z3.And(model[term_l] <= term, model[term_u] >= term))
+
+            cnts_dis.append(big_and(cnts))
+        return big_or(cnts_dis)
