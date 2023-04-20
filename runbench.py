73f36c418edd780fb577a966371bb74d579e80eb
windoctober <2908709191@qq.com>
2023-04-20 21:03:54 +0800

    test benchmark

--- runbench.py
+++ runbench.py
@@ -4,9 +4,12 @@ import subprocess
 from threading import Timer
 from typing import List
 import csv
+import shutil
 import json
 import itertools
 import signal
+from multiprocessing import Process,cpu_count,Queue
+
 
 
 LANG=["chc"]
@@ -20,6 +23,7 @@ all_template=["bv_interval", "power_bv_interval",
 MAXTIME=1
 GOAL_PATH="/Benchmark"
 Processed_PATH="/SafeBenchmark"
+ENDWITH='.smt2'
 
 #################### By KJY #########################
 # def csv_create(method,lang,results_list):
@@ -104,6 +108,25 @@ def parsing_out(file_path,template,lines,mode='kind'):
     
     return result_dict
 
+def data_process(results):
+    file_name=[]
+    save_safe_dir=CUR_DIR+Processed_PATH
+    origin_path=CUR_DIR+GOAL_PATH
+    if not os.path.isdir('SafeBenchmark'):
+        os.mkdir('SafeBenchmark')
+    for result in results:
+        if 'safe' not in result or not result['safe']==True:
+            continue
+        name=result['file']
+        if name in file_name:
+            continue
+        file_name.append(name)
+        relative_path=result['origin_relpath']
+        safe_path= os.path.join(save_safe_dir, relative_path)
+        if not os.path.exists(os.path.dirname(safe_path)):
+            os.makedirs(os.path.dirname(safe_path))
+        shutil.copy(os.path.join(origin_path, relative_path),os.path.join(save_safe_dir, relative_path))
+
 #####################################################
 
 
@@ -142,7 +165,7 @@ def solve_with_bin_solver(cmd: List[str], timeout: int) -> str:
         # return "timeout"
     return out
 
-
+  
 def solve_file(file_path,ef_template):
     if ef_template=='None':
         cmd = ["python3", CUR_DIR + "/prover.py", "--engine", METHOD, "--lang", LANG,"--file", file_path]
@@ -155,10 +178,11 @@ def solve_file(file_path,ef_template):
     
     
 
-def three_method_compare(root):
+def three_method_compare(root,num_of_thread):
     file_list=[]
-    result_list=[]
+    
     result_dir='./result'
+    
     if not os.path.isdir("result"):
         os.mkdir("result")
     # get all .smt2 [chc format]
@@ -167,32 +191,76 @@ def three_method_compare(root):
             if filename.endswith('.smt2'):
                 file_path = os.path.join(dirpath, filename)
                 file_list.append(file_path)
+                
+    if METHOD=='efsmt':
+        templates=all_template
+    else:
+        templates=['None']
     
-    for file_path in file_list:
-        relative_path = os.path.relpath(file_path, root)
-        no_ext_path, _ = os.path.splitext(relative_path)
-        new_path = os.path.join(result_dir, no_ext_path)
-        new_dir = os.path.dirname(new_path)
-        
-        if not os.path.exists(new_dir):
-            os.makedirs(new_dir)
-        
-        new_path+="_"+str(METHOD)+"_"+str(LANG)
-        if METHOD!='efsmt':
-            save_path=new_path+".json"
-            result = solve_file(file_path,'None')
-            with open(save_path, 'w') as f:
-                json.dump(result,f,indent=4)
-            result_list.append(result)
-        else:
-            for template in all_template:
+    def multi_func(files,result_queue):
+        result_list=[]
+        for file in files:
+            print(file)
+            relative_path = os.path.relpath(file, root)
+            no_ext_path, _ = os.path.splitext(relative_path)
+            new_path = os.path.join(result_dir, no_ext_path)
+            new_dir = os.path.dirname(new_path)
+            if not os.path.exists(new_dir):
+                os.makedirs(new_dir)
+            
+            for template in templates:
                 save_path=new_path+"_"+str(template)+".json"
-                result=solve_file(file_path,template)
+                result=solve_file(file,template)
+                result['origin_relpath']=relative_path
                 with open(save_path, 'w') as f:
                     json.dump(result,f,indent=4)
                 result_list.append(result)
+        result_queue.put(result_list)
     
-    # data_process(result_list)
+    num_of_file = len(file_list) //num_of_thread
+    remainder = len(file_list) % num_of_thread
+    processes = []
+    result_queue = Queue() 
+
+    for i in range(num_of_thread):
+        start_index = i * num_of_file + min(i, remainder)
+        end_index = (i + 1) * num_of_file + min(i + 1, remainder)
+        process = Process(target=multi_func, args=(file_list[start_index:end_index],result_queue))
+        process.start()
+        processes.append(process)
+    
+    all_results = []
+    for _ in range(num_of_thread):
+        all_results.extend(result_queue.get()) 
+        
+    for process in processes:
+        process.join()
+    # for file_path in file_list:
+    #     relative_path = os.path.relpath(file_path, root)
+    #     no_ext_path, _ = os.path.splitext(relative_path)
+    #     new_path = os.path.join(result_dir, no_ext_path)
+    #     new_dir = os.path.dirname(new_path)
+        
+        # if not os.path.exists(new_dir):
+        #     os.makedirs(new_dir)
+        
+    #     new_path+="_"+str(METHOD)+"_"+str(LANG)
+    #     if METHOD!='efsmt':
+    #         save_path=new_path+".json"
+    #         result = solve_file(file_path,'None')
+    #         with open(save_path, 'w') as f:
+    #             json.dump(result,f,indent=4)
+    #         result_list.append(result)
+    #     else:
+    #         for template in all_template:
+    #             save_path=new_path+"_"+str(template)+".json"
+    #             result=solve_file(file_path,template)
+    #             with open(save_path, 'w') as f:
+    #                 json.dump(result,f,indent=4)
+    #             result_list.append(result)
+    
+    
+    data_process(all_results)
     return
 
 def Initial():
@@ -214,8 +282,11 @@ if __name__ == "__main__":
     parser.add_argument('-p','--goal_path',default=GOAL_PATH)
     parser.add_argument('-ns','--nosafe',action='store_true')
     parser.add_argument('-t','--time',default=MAXTIME)
+    parser.add_argument('-th','--thread',default=cpu_count())
     args=parser.parse_args()
+    
     MAXTIME=int(args.time)
+    print("Used Thread Number",cpu_count())
     if not args.nosafe:
         GOAL_PATH=args.goal_path
         for Language,Method in itertools.product(args.lang,args.method):
@@ -223,5 +294,6 @@ if __name__ == "__main__":
             METHOD=Method
             run=Initial()
             if run:
-                three_method_compare(CUR_DIR + GOAL_PATH)
+                three_method_compare(CUR_DIR + GOAL_PATH, int(args.thread))
+    
     
