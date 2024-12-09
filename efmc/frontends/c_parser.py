310ebb669b1c8709f501bb7f14d48f968eada625
rainoftime <rainoftime@gmail.com>
2024-12-09 16:29:19 +0800

    first try of c_parser (failed..?)

--- efmc/frontends/c_parser.py
+++ efmc/frontends/c_parser.py
@@ -1,14 +1,47 @@
-"""Parser for C files"""
+from typing import Dict, Set
+
+import z3
+from pycparser import c_parser, c_ast
+
+from efmc.sts import TransitionSystem
 
 
-class CParser:
+class CToTransitionSystem:
+    def __init__(self):
+        self.parser = c_parser.CParser()
+        self.variables: Set[str] = set()
+        self.prime_variables: Set[str] = set()
+        self.init_conditions = []
+        self.trans_conditions = []
+        self.post_conditions = []
+        self.var_types: Dict[str, str] = {}
 
-    def __init__(self, inputs: str, to_real: bool):
-        return
+    def parse_program(self, c_code: str, post_condition: str = None) -> TransitionSystem:
+        raise NotImplementedError
 
-    def get_transition_system(self):
-        all_vars = []
-        init = None
-        trans = None
-        post = None
-        return all_vars, init, trans, post
+    def _process_ast(self, node: c_ast.Node):
+        raise NotImplementedError
+
+    def _handle_loop(self, node: c_ast.While):
+        raise NotImplementedError
+
+    def _process_declarations(self, decls):
+        raise NotImplementedError
+
+
+def simple_counter_loop():
+    c_code = """
+        int x = 0;
+        while (x < 10) {
+            x = x + 1;
+        }
+        """
+    post_condition = "x == 10"
+
+    parser = CToTransitionSystem()
+
+    ts = parser.parse_program(c_code, post_condition)
+
+
+
+simple_counter_loop()
