1d15ef058991d717459f923b143cb8be49426987
rainoftime <rainoftime@gmail.com>
2025-04-16 09:14:01 +0800

    fix ci

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ REQUIRED = [
     'tqdm~=4.65.0',
     'meson>=0.64',
     'pytest~=7.3.2',
-    'coverage~=6.0.0'
+    'coverage~=6.0.0',
+    'requests==2.32.3'
 ]
 
 # The rest you shouldn't have to touch too much :)
